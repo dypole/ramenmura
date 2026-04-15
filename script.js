6d4e99e94bf07a725354cf299c704f788452789f
dypole <dypole@gmail.com>
2026-04-15 18:05:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -21,4 +21,37 @@ function loadGallery() {
   });
 }
 
+/*=======온라인 오더========*/
+function doGet() {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Menu");
+  const data = sheet.getDataRange().getValues();
+
+  let result = {};
+
+  for (let i = 1; i < data.length; i++) {
+    let category = data[i][0];
+    let name = data[i][1];
+    let price = data[i][2];
+
+    if (!result[category]) result[category] = [];
+
+    result[category].push({ name, price });
+  }
+
+  return ContentService.createTextOutput(JSON.stringify(result));
+}
+
+function doPost(e) {
+  const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Orders");
+  const data = JSON.parse(e.postData.contents);
+
+  sheet.appendRow([
+    new Date(),
+    JSON.stringify(data.cart),
+    data.total
+  ]);
+
+  return ContentService.createTextOutput("success");
+}
+
 loadGallery();
